d1fa46e976e545bd2dd0620fc26f2482671a1d03
Gogo <gogoshedrack@gmail.com>
2025-12-15 13:28:00 +0100

    Rename CRUD templates to match check requirements

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -62,52 +62,27 @@ def profile_view(request):
 
 
 
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
-
-# List all posts
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'  # custom template
+    template_name = 'blog/listing.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-# Detail of a single post
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/viewing.html'
 
-# Create a new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/creating.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-# Update a post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/editing.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-# Delete a post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = '/'  # redirect after deletion
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+    template_name = 'blog/deleting.html'
+    success_url = '/'
