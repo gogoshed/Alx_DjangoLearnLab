24f80cf6abcfca34fc8439f6cd17daebbbde6ef3
Gogo <gogoshedrack@gmail.com>
2025-12-16 13:32:09 +0100

    Remove

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,25 +1,40 @@
 # blog/urls.py
+# from django.urls import path
+# from . import views
+# from .views import (
+#     PostListView,
+#     PostDetailView,
+#     PostCreateView,
+#     PostUpdateView,
+#     PostDeleteView,
+# )
+
+# urlpatterns = [
+#     path('register/', views.register_view, name='register'),
+#     path('login/', views.login_view, name='login'),
+#     path('logout/', views.logout_view, name='logout'),
+#     path('profile/', views.profile_view, name='profile'),
+#     path('posts/', PostListView.as_view(), name='post-list'),
+#     path('post/new/', PostCreateView.as_view(), name='post-create'),
+#     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+#     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+#     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+# ]
+
+
+
+
 from django.urls import path
-from . import views
 from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
+    PostListView, PostDetailView, PostCreateView, 
+    PostUpdateView, PostDeleteView
 )
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
-
-
 
