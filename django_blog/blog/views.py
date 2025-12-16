675f7c3d11de001b2af97e94c8723b183b550f4e
Gogo <gogoshedrack@gmail.com>
2025-12-16 13:03:39 +0100

    Move templates into blog folder and update template_name paths

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -76,19 +76,19 @@ from .forms import PostForm
 
 class PostListView(ListView):
     model = Post
-    template_name = 'listing.html'
+    template_name = 'blog/listing.html'
     context_object_name = 'posts'
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'viewing.html'
+    template_name = 'blog/viewing.html'
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'creating.html'
+    template_name = 'blog/creating.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -98,7 +98,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'editing.html'
+    template_name = 'blog/editing.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -109,7 +109,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'deleting.html'
+    template_name = 'blog/deleting.html'
     success_url = reverse_lazy('post-list')
 
     def test_func(self):
