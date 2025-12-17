7201fabd220eb55474450143e03914992fd5fc00
Gogo <gogoshedrack@gmail.com>
2025-12-17 22:26:29 +0100

    Register Book model with admin.ModelAdmin

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,21 +1,33 @@
-from django.db import models
+# from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField()
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
+
+
+# from django.contrib import admin
+# from .models import Book
+
+# # Register Book model with admin using ModelAdmin
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'publication_year')  # shows these fields in admin list view
+#     list_filter = ('author', 'publication_year')            # enables filter sidebar
+#     search_fields = ('title', 'author')                     # enables search box
+
+# admin.site.register(Book, BookAdmin)
+
 
 
 from django.contrib import admin
 from .models import Book
 
-# Register Book model with admin using ModelAdmin
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # shows these fields in admin list view
-    list_filter = ('author', 'publication_year')            # enables filter sidebar
-    search_fields = ('title', 'author')                     # enables search box
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
