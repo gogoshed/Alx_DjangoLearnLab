79a8f8f7e247570e4e8db1a28e8a2109cd055b7b
Gogo <gogoshedrack@gmail.com>
2025-12-18 19:58:34 +0100

    Clean up feed view and fix checker requirements

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics
 from rest_framework.pagination import PageNumberPagination
+
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -36,20 +36,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-from rest_framework import generics, permissions
-from .models import Post
-from .serializers import PostSerializer
+
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-
-        # REQUIRED by checker
         following_users = user.following.all()
-
-        # REQUIRED by checker
-        return Post.objects.filter(
-            author__in=following_users
-        ).order_by('-created_at')
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
