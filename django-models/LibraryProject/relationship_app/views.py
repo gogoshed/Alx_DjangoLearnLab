60feea860305fa5878e5411634d2f65c1746200b
Gogo <gogoshedrack@gmail.com>
2025-12-18 10:10:12 +0100

    Fix auth views to use django.contrib.auth login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,41 +1,71 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
+# from django.shortcuts import render
+# from django.views.generic.detail import DetailView
 
-from .models import Library
-from .models import Book
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+# from .models import Library
+# from .models import Book
 
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+# def list_books(request):
+#     books = Book.objects.all()
+#     return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+# class LibraryDetailView(DetailView):
+#     model = Library
+#     template_name = "relationship_app/library_detail.html"
+#     context_object_name = "library"
+
+
+
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.views import LoginView, LogoutView
+# from django.contrib.auth.forms import UserCreationForm
+
+
+
+# def register(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')
+#     else:
+#         form = UserCreationForm()
+
+#     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+
+
+
+# class CustomLoginView(LoginView):
+#     template_name = 'relationship_app/login.html'
+
+
+# class CustomLogoutView(LogoutView):
+#     template_name = 'relationship_app/logout.html'
+
+
 
 
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login
 
 
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('login')
     else:
         form = UserCreationForm()
 
     return render(request, 'relationship_app/register.html', {'form': form})
-
-
-
 
 
 class CustomLoginView(LoginView):
