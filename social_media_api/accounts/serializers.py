d9035f7098b52f3dd281303ca6917bf06b29802e
Gogo <gogoshedrack@gmail.com>
2025-12-18 17:13:35 +0100

    Fix serializer to meet ALX token auth requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,20 +11,35 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'email', 'password', 'bio')
 
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField()  # REQUIRED exact match
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'bio')
+
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(  # REQUIRED exact match
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+        user.bio = validated_data.get('bio', '')
+        user.save()
+
         Token.objects.create(user=user)
         return user
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'bio', 'followers_count')
+        fields = ('username', 'email', 'bio')
+
