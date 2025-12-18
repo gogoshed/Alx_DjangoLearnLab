3f70eabb129c9be403e8ed96e0bcea09cc41f284
Gogo <gogoshedrack@gmail.com>
2025-12-18 12:01:14 +0100

    Fix urls.py: remove duplicates, add add/edit/delete book paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 # relationship_app/urls.py
 from django.urls import path
-from . import views  # import all views as a module
+from . import views
 
 urlpatterns = [
     # Role-Based Views
@@ -17,11 +17,8 @@ urlpatterns = [
     # Library Detail
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Auth Views
+    # Authentication
     path('register/', views.register, name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
 ]
