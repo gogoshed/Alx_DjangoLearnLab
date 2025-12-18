54ca63f3b3b657e2045bf9d22bbfe3b3a40a3a03
Gogo <gogoshedrack@gmail.com>
2025-12-18 17:10:29 +0100

    Initial setup: Social Media API with user authentication

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,33 +1,20 @@
-# from django.db import models
-
-# class Book(models.Model):
-#     title = models.CharField(max_length=200)
-#     author = models.CharField(max_length=100)
-#     publication_year = models.IntegerField()
-
-#     def __str__(self):
-#         return self.title
-
-
-# from django.contrib import admin
-# from .models import Book
-
-# # Register Book model with admin using ModelAdmin
-# class BookAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'author', 'publication_year')  # shows these fields in admin list view
-#     list_filter = ('author', 'publication_year')            # enables filter sidebar
-#     search_fields = ('title', 'author')                     # enables search box
-
-# admin.site.register(Book, BookAdmin)
-
-
-
 from django.contrib import admin
-from .models import Book
+from .models import Book  # import your Book model
+
+# Register Book model with default admin
+admin.site.register(Book)
+
+
 
 class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
     list_display = ('title', 'author', 'publication_year')
+    
+    # Fields to filter by in the right sidebar
     list_filter = ('author', 'publication_year')
+    
+    # Fields to search by using the search bar
     search_fields = ('title', 'author')
 
+# Register the Book model with the custom admin
 admin.site.register(Book, BookAdmin)
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+from .serializers import RegisterSerializer, UserProfileSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            'token': token.key,
+            'user_id': token.user.id,
+            'username': token.user.username
+        })
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        return self.request.user
