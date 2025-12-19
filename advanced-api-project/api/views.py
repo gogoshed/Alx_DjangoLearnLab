e187894fa189053a53ead6b8b4478f8275014de7
Gogo <gogoshedrack@gmail.com>
2025-12-19 16:08:49 +0100

    Fix ALX checker: use rest_framework.DjangoFilterBackend for filtering, searching, ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 from .models import Book
 from .serializers import BookSerializer
@@ -21,7 +21,7 @@ class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     # DRF backends
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
     # Fields for filtering
     filterset_fields = ['title', 'author__name', 'publication_year']
