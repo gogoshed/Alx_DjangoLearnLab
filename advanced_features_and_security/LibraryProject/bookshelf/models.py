f368153717f7591cae6b55518430b2a19f1c2fa1
Gogo <gogoshedrack@gmail.com>
2025-12-19 12:48:01 +0100

    Add Book model with custom permissions can_create, can_delete

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -37,3 +37,23 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+from django.db import models
+from django.utils import timezone
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(default=timezone.now)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
