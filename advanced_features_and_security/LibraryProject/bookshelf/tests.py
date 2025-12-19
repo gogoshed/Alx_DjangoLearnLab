9f01f4ebc1db968d1e7ab5b22000155d009427cb
Gogo <gogoshedrack@gmail.com>
2025-12-19 12:50:15 +0100

    Add book_list view and permission decorators

--- advanced_features_and_security/LibraryProject/bookshelf/tests.py
+++ advanced_features_and_security/LibraryProject/bookshelf/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('books/create/', views.create_book, name='create_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+]
